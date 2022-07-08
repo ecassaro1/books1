faec496d2aa9bb52b9a83cfcbdc1d2b285787f86
ecassaro1 <ericcassaro@yahoo.com>
2022-07-08 16:18:12 -0300

    styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import React, {useState} from 'react';
 import List from './List.js';
 import Form from './Form.js';
+import { styles } from './styles';
+
+const divStyle = {
+  background: styles.background,
+  height: '100%',
+  color: styles.color
+};
 
 function App() {
   const [newTimestamp,setNewTimestamp] = useState(new Date().getTime());
@@ -12,7 +19,7 @@ function App() {
 
   return (
     <>
-      <div id = "App" className="App">
+      <div id = "App" className="App" style={divStyle}>
         <h3>Biblioteca</h3>
         <br/>
         <div>
---
+++ src/styles.js
@@ -0,0 +1,13 @@
+const fontFamily = 'Comic Sans MS, Lucida Handwriting, cursive';
+const background = 'pink url("https://thumbs.dreamstime.com/z/blue-wireframe-design-nice-reflection-d-rendering-background-wallpaper-abstract-black-tunnel-illustrtion-156249808.jpg") fixed';
+const fontSize = '4em';
+const padding = '45px 0';
+const color = '#aec5eb';
+
+export const styles = {
+  fontFamily: fontFamily,
+  background: background,
+  fontSize: fontSize,
+  padding: padding,
+  color: color
+};
